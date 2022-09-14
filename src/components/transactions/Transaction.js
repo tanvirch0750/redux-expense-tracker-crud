099dc450f17a83d9ca8868fdfdff06ada25c97d9
tanvirch0750 <tanvir0750@yahoo.com>
2022-09-14 20:53:39 +0600

    implemented fetch transaction

--- src/components/transactions/Transaction.js
+++ src/components/transactions/Transaction.js
@@ -1,12 +1,12 @@
 import EditImg from '../../assets/images/edit.svg';
 import DelteImg from '../../assets/images/delete.svg';
 
-const Transaction = () => {
+const Transaction = ({ transaction }) => {
   return (
-    <li className="transaction income">
-      <p>Earned this month</p>
+    <li className={`transaction ${transaction.type}`}>
+      <p>{transaction.name}</p>
       <div className="right">
-        <p>৳ 100</p>
+        <p>৳ {transaction.amount}</p>
         <button className="link">
           <img className="icon" src={EditImg} alt="edit" />
         </button>
